a0f74136dbaae474849c3e3c51f80c6d1a67855e
prakasitz <prakasitbm@gmail.com>
2022-11-28 20:35:37 +0700

    udate code try findOneAndUpdate

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,126 +1,121 @@
-import { Inject, Injectable, UseFilters  } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { InjectModel } from "@nestjs/mongoose";
-import { InjectDataSource, InjectRepository } from "@nestjs/typeorm";
+import { Inject, Injectable, UseFilters } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
 import { plainToClass } from 'class-transformer';
-import { ValidationError } from "class-validator";
-import { Model } from "mongoose";
-import { AppService } from "src/app.service";
-import { User } from "src/entity/user.entity";
-import { DataSource, ObjectID, Repository } from "typeorm";
-import { createUserDto, hobby, hobbyUpdate } from "./dto/CreateUser.dto";
-import { IUser } from "./IUser";
+import { ValidationError } from 'class-validator';
+import { Model } from 'mongoose';
+import { AppService } from 'src/app.service';
+import { User } from 'src/entity/user.entity';
+import { DataSource, ObjectID, Repository } from 'typeorm';
+import { createUserDto, hobby, hobbyUpdate } from './dto/CreateUser.dto';
+import { IUser } from './IUser';
 // import { User, User as UserMongoose, UserDocument, UserSchema  as UserSchemaMongoose} from './schemas/user.mongoose.schema';
-import { ObjectId } from "mongodb";
-import { Hobby } from "./schemas/hobby.schema";
-import { Hobbys } from "src/entity/Hobby.model";
+import { ObjectId } from 'mongodb';
+import { Hobby } from './schemas/hobby.schema';
+import { Hobbys } from 'src/entity/Hobby.model';
 
-
-@Injectable() 
+@Injectable()
 export class UserService {
     constructor(
-        // @InjectModel(UserMongoose.name, 'userMongoose') 
+        // @InjectModel(UserMongoose.name, 'userMongoose')
         // private userRepository: Model<UserDocument>,
-        
 
-        @InjectRepository(User, 'userConnection') 
+        @InjectRepository(User, 'userConnection')
         private userRepository: Repository<User>,
-        
-        @InjectDataSource('userConnection') 
+
+        @InjectDataSource('userConnection')
         private testDb: DataSource,
-
-
-    ) {
-    }
-
+    ) {}
 
     async getAllUser() {
         return this.userRepository.find();
     }
 
-    async updateHobbys({userId, hobbys}: hobbyUpdate) {
-        let income_new_arr = hobbys.filter((item) => {
-            if(!item.id) return item 
-        })
-        income_new_arr.map((item) => {
-            item.id = new ObjectId()
-            item.createDate = new Date()
-        })
+    async updateHobbys({ userId, hobbys }: hobbyUpdate) {
+        let income_new_arr = [];
+        let income_exsit_arr = [];
+        hobbys.forEach((item) => {
+            if (!item.id) {
+                income_new_arr.push({
+                    id: new ObjectId(),
+                    createDate: new Date(),
+                    ...item,
+                });
+            } else {
+                income_exsit_arr.push(item);
+            }
+        });
 
-        let income_exsit_arr = hobbys.filter((item) => {
-            if(item.id) return item 
-        })
-        
-        const usersRepository = this.testDb.getMongoRepository(User)
-        const target = await usersRepository.findOneBy({
-            "_id": new ObjectId(userId)
-        })
+        const query: object = { _id: new ObjectId(userId) };
 
-        console.log('1', target.hobbys)
+        console.log('income_new_arr', income_new_arr);
+        console.log('income_exsit_arr', income_exsit_arr);
 
-        target.hobbys.forEach((item, index) => {
+        const usersRepository = this.testDb.getMongoRepository(User);
+        const target = await usersRepository.findOneBy(query);
+
+        console.log('data: before', target.hobbys);
+
+        target.hobbys.forEach((item) => {
             income_exsit_arr.forEach((el) => {
-                if(item.id != el.id) return;
-                if(item.name != el.name ||
-                    item.frequency != el.frequency) {
-                    item.name = el.name
-                    item.frequency = el.frequency
-                    item.updateDate = new Date()
+                if (item.id != el.id) return;
+                if (item.name != el.name || item.frequency != el.frequency) {
+                    item.name = el.name;
+                    item.frequency = el.frequency;
+                    item.updateDate = new Date();
                 }
-            })
-        })
+            });
+        });
 
-        console.log('2', target.hobbys)
-        console.log('------end check--------')
-
+        console.log('data: after updated', target.hobbys);
+        console.log('------end check--------');
 
         let options = {
             projection: {
-                "hobbys": 1
+                hobbys: 1,
             },
             returnDocument: 'after',
-        }
+        };
 
-        const aa = await usersRepository.findOneAndUpdate({
-            _id: new ObjectId(userId)
-        },{
-            $set: {
-                "hobbys": target.hobbys
-            }
-        }, options)
-
-
-        let query = {
-            _id: new ObjectId(userId)
-        }
-
-        let updateObj = {
-                $push: { 
-                    "hobbys": {$each: [...income_new_arr]}
+        const resultOfPush = await usersRepository.findOneAndUpdate(
+            query,
+            {
+                $set: {
+                    hobbys: target.hobbys,
                 },
-            }
+            },
+            options,
+        );
 
-        const result = await usersRepository.findOneAndUpdate(query, updateObj, options)
-        console.log(aa.value)
-        console.log('-------------------')
-        console.log(result.value)
-        return result.value
-
+        const resultOfUpdated = await usersRepository.findOneAndUpdate(
+            query,
+            {
+                $push: {
+                    hobbys: { $each: [...income_new_arr] },
+                },
+            },
+            options,
+        );
+        console.log('-------------------');
+        console.log('resultOfPush', resultOfPush.value);
+        console.log('resultOfUpdated', resultOfUpdated.value);
+        console.log('-------------------');
+        
+        return resultOfUpdated.value;
     }
 
     async createNewUser(user: any) {
         try {
             const createdUser = this.userRepository.save(user);
-            return createdUser
+            return createdUser;
         } catch (err) {
-            console.log(err)
+            console.log(err);
             return {
-                error: err.message
-            }
+                error: err.message,
+            };
         }
 
         //return await this.userRepository.save();
     }
-
-
 }
